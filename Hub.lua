3e18fd0764d86a1b3bd4e911e01aaca5d3746582
arkanzulfadliputra-oss <arkanzulfadliputra@gmail.com>
2026-04-01 16:23:59 +0800

    Enhance GUI with new buttons for custom items
    Added new buttons for custom entities and items in the GUI.

---
+++ Hub.lua
@@ -0,0 +1,232 @@
+-------NOTIFY----------------------------------------NOTIFY-----------------------
+local StarterGui = game:GetService("StarterGui")
+
+for i = 1, 5 do
+	game.StarterGui:SetCore("SendNotification", {
+		Title = "SCRIPTER BY @ScripterGaming",
+		Text = "Collab @ScripterGaming",
+		Duration = 5
+	})
+	task.wait(1.3)
+end
+------Load----------------------------------------load------------------
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TwiRwal/TwiWare/main/uilib.lua"))() -- open source
+
+local Window = Library.CreateLib("Script Gaming Hub V1 (Script Expired)", "BloodTheme")
+local Tab = Window:NewTab("Custom Entities")
+local Section = Tab:NewSection("Custom Entities Regular Vynixus")
+Section:NewButton("Rebound Remake", "Have Fun", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/aDnvLtc0"))()
+end)
+
+Section:NewButton("Ripper", "Have Fun", function()
+     loadstring(game:HttpGet("https://pastebin.com/raw/duAAB2tp"))()
+end)
+
+Section:NewButton("A-60", "Have Fun", function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/A-60.lua"))()
+end)
+
+Section:NewButton("Silence", "Have Fun", function()
+      loadstring(game:HttpGet("https://pastebin.com/raw/FRs4rBed"))()
+end)
+
+Section:NewButton("X-60", "Have Fun", function()
+      loadstring(game:HttpGet("https://pastebin.com/raw/rCS3K95T"))()
+end)
+
+Section:NewButton("B-60", "Have Fun", function()
+      loadstring(game:HttpGet("https://pastebin.com/raw/EyH5nC1t"))()
+end)
+
+Section:NewButton("Surge", "ButtonInfo", function()
+      loadstring(game:HttpGet("https://pastebin.com/raw/yuQDrnUJ"))()
+end)
+
+Section:NewButton("Ambush Nightmare", "ButtonInfo", function()
+      loadstring(game:HttpGet("https://pastebin.com/raw/StsBUgzF"))()
+end)
+
+Section:NewButton("Rebound Endless", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/6KtmYCAQ"))()
+end)
+
+Section:NewButton("Greed", "ButtonInfo", function()
+      loadstring(game:HttpGet("https://pastebin.com/raw/3mquCrkF"))()
+end)
+
+Section:NewButton("A-60 Remake", "ButtonInfo", function()
+      loadstring(game:HttpGet("https://pastebin.com/raw/UGZVqiQ5"))()
+end)
+
+Section:NewButton("Rebound Hardcore New", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://pastebin.com/raw/2RwQUQRt"))()
+end)
+
+Section:NewButton("Rebound Old", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://pastebin.com/raw/MqUhjTVJ"))()
+end)
+
+Section:NewButton("Trolololo Ambush", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/TROLL%20AMBUSH.lua"))()
+end)
+
+Section:NewButton("Kawaii Ambush", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://pastebin.com/raw/VHqii3zx"))()
+end)
+
+Section:NewButton("OLD Ambush", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/OLD%20AMBUSH.lua"))()
+end)
+
+Section:NewButton("Hunger By Me", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/Hunger.lua"))()
+end)
+
+Section:NewButton("A-60 V2", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/A-60%20V2.lua"))()
+end)
+
+Section:NewButton("A-60 Hardcore", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/A-60%20HARDCORE.lua"))()
+end)
+
+Section:NewButton("LSPLASH", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/LSPLASH.lua"))()
+end)
+
+Section:NewButton("Happy Munci", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/HAPPY%20MUNCI.lua"))()
+end)
+
+Section:NewButton("TrollFace", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/Troll%20Face.lua"))()
+end)
+
+Section:NewButton("White", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/White.lua"))()
+end)
+
+Section:NewButton("Smiler", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/Smiler.lua"))()
+end)
+
+Section:NewButton("Trauma", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/Trauma.lua"))()
+end)
+
+Section:NewButton("Rage", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/Rage.lua"))()
+end)
+
+Section:NewButton("Hunger", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/Hungered.lua"))()
+end)
+
+Section:NewButton("FireAmbush", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/Fire.lua"))()
+end)
+
+Section:NewButton("A-25", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/A-25.lua"))()
+end)
+
+Section:NewButton("Eater", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/Eater.lua"))()
+end)
+
+Section:NewButton("Haste", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://pastebin.com/raw/qj4gAiRF"))()
+end) 
+
+Section:NewButton("Depth", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://pastebin.com/raw/EWz5Espp"))()
+end)
+
+Section:NewButton("Firebrand", "ButtonInfo", function()
+    print("Clicked")
+    loadstring (game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/Entities-Models/refs/heads/main/Custom%20Entities%20Models/Firebrand.lua"))()
+end)
+
+Section:NewButton("TrollRush", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/Entities-Models/refs/heads/main/Custom%20Entities%20Models/Troll%20Rush.lua"))()
+end)
+
+Section:NewButton("Unknown", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/Entities-Models/refs/heads/main/Custom%20Entities%20Models/%3F%3F%3F%3F%3F.lua"))()
+end)
+
+Section:NewButton("Big Games", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/Entities-Models/refs/heads/main/Custom%20Entities%20Models/Big%20games.lua"))()
+end)
+
+Section:NewButton("E-2", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/CUSTOM%20ENTITIES/E-2.lua"))()
+end)
+
+Section:NewButton("B-60 PHOTON", "ButtonInfo", function()
+    print("Clicked")
+end)
+
+local Tab = Window:NewTab("Custom Item")
+local Section = Tab:NewSection("Coming Soon in 2024")
+
+Section:NewButton("Crucifix All", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/Eyes%20Crucifix.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/%3F%3F%3F%20Crucifix.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Ambush%20Crucifix.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/seek%20Crucifix.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Crucifix%20Blue.lua"))()
+end)
+
+Section:NewButton("Rebound Plushie (Still Bug)", "ButtonInfo", function()
+    print("Clicked")
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/Custom%20Item/Rebound%20Plushie"))()
+end)
+
+Section:NewButton("A-60 Plushie", "ButtonInfo", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/Custom%20Item/A-60%20Plushie"))()
+end)
+
+Section:NewButton("Guiding Candle", "ButtonInfo", function()
+    local tool = LoadCustomInstance("rbxassetid://12093585726")
+tool.GripPos = Vector3.new(0, -0.5, 0)
+tool.Parent = game.Players.LocalPlayer.Backpack
+end)
+
+Section:NewButton("Crucifix", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/ScriptGamingGamer/refs/heads/main/Script/DOORS/Crucifix.lua"))()
+end)
+
+local Tab = Window:NewTab("Toggle Gui")
+local Section = Tab:NewSection("Toggle Gui")
+Section:NewKeybind("Toggle Gui", "KeybindInfo", Enum.KeyCode.F, function()
+	Library:ToggleUI()
+end)
