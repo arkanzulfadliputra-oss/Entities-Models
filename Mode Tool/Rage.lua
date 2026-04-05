310d249b700d3cd7fce8b5bdb34181162fc57afb
arkanzulfadliputra-oss <arkanzulfadliputra@gmail.com>
2026-04-06 07:17:16 +0800

    Create Rage.lua

---
+++ Mode Tool/Rage.lua
@@ -0,0 +1,130 @@
+local ColorCorrection = Instance.new("ColorCorrectionEffect", game.Lighting)
+ColorCorrection.TintColor = Color3.fromRGB(255, 0, 0)
+ColorCorrection.Contrast = 0.2
+ColorCorrection.Saturation = -0.5
+local tween = game:GetService("TweenService")
+tween:Create(ColorCorrection, TweenInfo.new(5), {Contrast = 0}):Play()
+tween:Create(ColorCorrection, TweenInfo.new(5), {Saturation = 0}):Play()
+local TW = tween:Create(ColorCorrection, TweenInfo.new(5), {TintColor = Color3.fromRGB(255, 255, 255)}) -- Balik ke putih
+TW:Play()
+wait(2.8)
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
+
+---====== Create entity ======---
+
+local entity = spawner.Create({
+	Entity = {
+		Name = "Rage",
+		Asset = "https://github.com/arkanzulfadliputra-oss/Entities-Models/raw/main/Entity%20Model/Rage.rbxm",
+		HeightOffset = 0
+	},
+	Lights = {
+		Flicker = {
+			Enabled = true,
+			Duration = 7
+		},
+		Shatter = true,
+		Repair = false
+	},
+	Earthquake = {
+		Enabled = false
+	},
+	CameraShake = {
+		Enabled = true,
+		Range = 100,
+		Values = {1.5, 20, 0.1, 1} -- Magnitude, Roughness, FadeIn, FadeOut
+	},
+	Movement = {
+		Speed = 600,
+		Delay = 7,
+		Reversed = false
+	},
+	Rebounding = {
+		Enabled = true,
+		Type = "Ambush", -- "Blitz"
+		Min = 1,
+		Max = 9,
+		Delay = 2
+	},
+	Damage = {
+		Enabled = true,
+		Range = 100,
+		Amount = 125
+	},
+	Crucifixion = {
+		Enabled = true,
+		Range = 40,
+		Resist = false,
+		Break = true
+	},
+	Death = {
+		Type = "Guiding", -- "Curious"
+		Hints = {"You Die By Rage", "You Have to Hide Quickly", "Come Revive To Survive ", "Thank You For Playing"},
+		Cause = "Rage"
+	}
+})
+
+---====== Debug entity ======---
+
+entity:SetCallback("OnSpawned", function()
+    print("Entity has spawned")
+end)
+
+entity:SetCallback("OnStartMoving", function()
+    print("Entity has started moving")
+end)
+
+entity:SetCallback("OnEnterRoom", function(room, firstTime)
+    if firstTime == true then
+        print("Entity has entered room: ".. room.Name.. " for the first time")
+    else
+        print("Entity has entered room: ".. room.Name.. " again")
+    end
+end)
+
+entity:SetCallback("OnLookAt", function(lineOfSight)
+	if lineOfSight == true then
+		print("Player is looking at entity")
+	else
+		print("Player view is obstructed by something")
+	end
+end)
+
+entity:SetCallback("OnRebounding", function(startOfRebound)
+    if startOfRebound == true then
+        print("Entity has started rebounding")
+	else
+        print("Entity has finished rebounding")
+	end
+end)
+
+entity:SetCallback("OnDespawning", function()
+    print("Entity is despawning")
+end)
+
+entity:SetCallback("OnDespawned", function()
+    print("Entity has despawned")
+end)
+
+entity:SetCallback("OnDamagePlayer", function(newHealth)
+	if newHealth == 0 then
+		print("Entity has killed the player")
+	else
+		print("Entity has damaged the player")
+	end
+end)
+
+--[[
+
+DEVELOPER NOTE:
+By overwriting 'CrucifixionOverwrite' the default crucifixion callback will be replaced with your custom callback.
+
+entity:SetCallback("CrucifixionOverwrite", function()
+    print("Custom crucifixion callback")
+end)
+
+]]--
+
+---====== Run entity ======---
+
+entity:Run()
