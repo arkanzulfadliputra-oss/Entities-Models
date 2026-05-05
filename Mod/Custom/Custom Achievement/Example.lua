79cc112201a7e9135e3f29ec500f6aab07cca0bc
arkanzulfadliputra-oss <arkanzulfadliputra@gmail.com>
2026-05-05 11:12:42 +0800

    Add Example achievement creation script

---
+++ Mod/Custom/Custom Achievement/Example.lua
@@ -0,0 +1,8 @@
+local AchievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/arkanzulfadliputra-oss/Entities-Models/refs/heads/main/Mod/Custom/Custom%20Achievement/SSource.lua"))()
+
+AchievementGiver.Giver.Create({
+    Title = "Example",
+    Desc = "Example",
+    Reason = "Example",
+    Image = "rbxassetid://117338032296497"
+})
